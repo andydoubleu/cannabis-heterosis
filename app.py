880deb64562778f860e9c46a72e9aed874d4dd33
Andy Wright <andy@atlasseed.com>
2025-04-10 14:46:47 -0700

    Update app.py

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ st.markdown(custom_css, unsafe_allow_html=True)
 # Atlas Lab logo
 st.image("atlas seed logo.png", width=120)
 
-st.title("🌿 Atlas Lab: Cannabis Heterosis Predictor")st.markdown("""
+st.title("🌿 Atlas Lab: Cannabis Heterosis Predictor") st.markdown("""
 Welcome to **Atlas Lab**!
 
 Upload genome CSV files for two parent strains to predict the potential level of heterosis in their progeny.
